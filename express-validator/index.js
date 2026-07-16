ff4c1c5a06c56bbf2a2a6ff081b6c9d5d0bf54a9
Ketan Rakesh <rakeshketan28@gmail.com>
2026-07-16 18:35:11 +0530

    add express-validator

---
+++ express-validator/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const {query, validationResult, matchedData} = require('express-validator');
+const app = express();
+app.use(express.json());
+app.get('/hello', query('person').notEmpty().escape(), (req, res) => {
+    const result = validationResult(req);
+    if (result.isEmpty()){
+        const data = matchedData(req);
+        return res.send(`Hello, ${data.person}!`);
+    }
+    res.send({errors: result.array()});
+})
+app.listen(3000);
